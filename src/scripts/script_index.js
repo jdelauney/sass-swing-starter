6d11dbf58522b2ab21d4be2e3bb11acc9757bcf8
jdelauney <delauney.jerome@gmail.com>
2022-09-11 16:41:24 +0200

    fix: fix initialisation in script_index.js

--- src/scripts/script_index.js
+++ src/scripts/script_index.js
@@ -2,13 +2,11 @@ import * as SassSwing from "sass-swing";
 
 /* Initialize all Sass Swing's components */
 (() => {
-	document.addEventListener('resize', SassSwing.debounce(SassSwing.fixDocumentHeight));
 	window.addEventListener("DOMContentLoaded", () => {
 		SassSwing.ThemeSwitcher.bind();
 		SassSwing.Tabify.bind();
 		SassSwing.Dropdownify.bind();
 		SassSwing.Slidify.bind();
 		SassSwing.Revealify.bind();
-		SassSwing.fixDocumentHeight();
 	});
 })();
